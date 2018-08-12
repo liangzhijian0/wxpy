f2ac66180d75d05288c5373556fa0cd2db44eaf6
Ocean Liang <657175454@qq.com>
2018-08-12 19:20:17 +0800

    send different city weather in the same time

--- 向群聊发信息.py
+++ 向群聊发信息.py
@@ -7,7 +7,9 @@ Created on Sun Aug 12 18:04:35 2018
 from wxpy import *
 
 bot = Bot() 
-wxpy_groups = bot.groups().search('这么多年的兄弟')[0]
+wenzi = bot.friends().search(u'MM7')[0]
+wxpy_groups = bot.groups().search('Dino')[0]
 print(type(wxpy_groups))
 print(wxpy_groups)
+wenzi.send("how are you")
 wxpy_groups.send("how are you")
--- 定点发信息.py
+++ 定点发信息.py
@@ -17,7 +17,7 @@ def DeltaSeconds():
     SECONDS_PER_DAY = 24 * 60 * 60
     
     curTime = datetime.now()
-    desTime = curTime.replace(hour=17, minute=56, second=0, microsecond=0)  #这里添加时间
+    desTime = curTime.replace(hour=18, minute=42, second=0, microsecond=0)  #这里添加时间
     delta = desTime - curTime
     skipSeconds = delta.total_seconds() % SECONDS_PER_DAY
     print ("Must sleep %d seconds" % skipSeconds)
@@ -31,10 +31,15 @@ while True:
     
     try:
         #需要发送的微信号    
-        #my_friend = bot.friends().search(u'蛋酥超人')[0]
-        wxpy_groups = bot.groups().search('这么多年的兄弟', ['杜学安'])
+        wenzi = bot.friends().search(u'A,bb')[0]
+        primary_group = bot.groups().search('这么多年的兄弟')[0]
+        
+       # Dino_group = bot.groups().search('Dino')[0]
+        FoShanList = [wenzi]
+        GuangZhouList = [primary_group,wenzi]
+       # ZhuHaiList = [Dino_group]
          
-        def showmsg(js):
+        def sendWeather(js,list):
             #显示时间
             date=js['result']['realtime']
             str = '地点:{0} 现在时间：{1} 农历：{2}'.format(date['city_name'],date['date'],date['moon']+'\n')
@@ -46,21 +51,41 @@ while True:
                 a = (k,':','天气情况:{0} 最高气温:{1}'.format(v[1],v[2]))
                 b = "".join(tuple(a))
                 str += b + '\n'
+                
             print(str)
-            my_friend.send(str)
+            for i in list:
+                i.send(str)
+            
         
     
         appkey='b38672c8cfa4434eaa1c2418592b39ad'
-        city='珠海'
-        value={
-            'key':appkey,
-            'cityname':city,
-        }
         url='http://api.avatardata.cn/Weather/Query'
-        s=requests.get(url,params=value)
-        js=s.json()
-        showmsg(js)     
-     
+        
+        FoShanValue={
+            'key':appkey,
+            'cityname':'佛山',
+        }
+        
+        GuangZhouValue={
+            'key':appkey,
+            'cityname':'广州',
+        }
+        
+        ZhuHaiValue={
+            'key':appkey,
+            'cityname':'珠海',
+        }
+        
+            
+        FoShanJs = requests.get(url,params=FoShanValue).json()
+        GuangZhouJs = requests.get(url,params=GuangZhouValue).json()
+        ZhuHaiJs = requests.get(url,params=ZhuHaiValue).json()
+        
+        sendWeather(FoShanJs,FoShanList) #发送佛山天气  
+        #sendWeather(GuangZhouJs,GuangZhouList) #发送广州天气
+        #sendWeather(ZhuHaiJs,ZhuHaiList) #发送珠海天气
+        
+        
     except:
         # 你的微信名称，不是微信帐号。
         my_friend = bot.friends().search('A,bb')[0]
